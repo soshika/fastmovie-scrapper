004c1d834b61bee61bc025552be7aaf14e557507
Sobhan Shafiei <soshika1010@gmail.com>
2022-06-12 19:57:56 +0430

    ready to go

--- digimovie-moviedownloadScrapper.py
+++ digimovie-moviedownloadScrapper.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     for page in range(1, 293):
         print("Page {0}".format(page) + ('-'*60))
         
-        url = 'https://digimovie.city/genre/درام/'
+        url = 'https://digimovie.li/genre/درام/'
 
         if page > 1 : 
             url += 'page/{0}/'.format(page)
@@ -30,28 +30,27 @@ if __name__ == "__main__":
             ind = 1
             for item in download_data:
                 a = item.find('a', class_='btn_row btn_dl')['href']
+                print('link no {0}: {1} '.format(ind, a))
+                download_url = str(a)
+                print('download is about to start : {0}'.format(download_url))
+                r = requests.get(download_url)
+                movie_name = download_url.split('/')[-1].replace('DigiMoviez', 'fastMovie')
+                with open(movie_name, 'wb') as f:
+                    f.write(r.content)
 
-                if '480' in a:
-                    print('link no {0}: {1} '.format(ind, a))
-                    download_url = str(a)
-                    print('download is about to start : {0}'.format(download_url))
-                    r = requests.get(download_url)
-                    movie_name = download_url.split('/')[-1].replace('DigiMoviez', 'fastMovie')
-                    with open(movie_name, 'wb') as f:
-                        f.write(r.content)
+                print('File {0} Downloaded Successfully'.format(movie_name))
 
-                    print('File {0} Downloaded Successfully'.format(movie_name))
+                directory = directory = os.getcwd()
+                file_path = directory + '/' + movie_name
+                
+                client = skynet.SkynetClient() # link to skynet
+                skylink = client.upload_file(file_path)
+                print("File {0} Uploaded successfully: link is {1} ".format(movie_name, skylink))
 
-                    directory = directory = os.getcwd()
-                    file_path = directory + '/' + movie_name
-                    
-                    client = skynet.SkynetClient() # link to skynet
-                    skylink = client.upload_file(file_path)
-                    print("File {0} Uploaded successfully: link is {1} ".format(movie_name, skylink))
-
-                    os.remove(file_path)
-                    print("File {0} deleted from server successfully".format(movie_name))
-                    #https://siasky.net/EAAATkfooqnDL-xjmleOf-gwXXLXIsFCWz74hJtxKk-i0Q
+                os.remove(file_path)
+                print("File {0} deleted from server successfully".format(movie_name))
+                #https://siasky.net/EAAATkfooqnDL-xjmleOf-gwXXLXIsFCWz74hJtxKk-i0Q
                 ind +=1
+                print('*'*50)
 
             
