5355b3304dfc0a619aae39d03db1dcee5160a20e
Sobhan Shafiei <soshika1010@gmail.com>
2022-06-27 18:19:46 +0430

    debug

--- cnama-downloader.py
+++ cnama-downloader.py
@@ -4,8 +4,8 @@ import os
 import wget
 import siaskynet as skynet
 
-def download_upload(link):
-    print('download is about start from page {0}'.format(row[2]))
+def download_upload(link,file_name):
+    print('download is about start  {0}'.format(file_name))
     movie_name = wget.download(link)
     print('\nFile {0} Downloaded Successfully'.format(movie_name))
 
@@ -22,7 +22,7 @@ def download_upload(link):
     os.remove(file_path)
     print("File {0} deleted from server successfully".format(movie_name))
 
-    movieDB.InsertTableEmi(skylink, row[2])
+    movieDB.InsertTableEmi(skylink, file_name)
     print("Inserted into DB Successfully")
     print('-'*50)
 
@@ -46,17 +46,17 @@ if __name__ == "__main__":
         if size <= 2.50:
             if '1080' in info.headers['Content-Disposition'] and '{0}-1080'.format(row[2]) not in dp:
                 dp['{0}-1080'.format(row[2])] = True
-                download_upload(link)
+                download_upload(link, row[2])
                 movieDB.delete_task(row[0])
             
             elif '720' in info.headers['Content-Disposition'] and '{0}-720'.format(row[2]) not in dp:
                 dp['{0}-720'.format(row[2])] = True
-                download_upload(link)
+                download_upload(link, row[2])
                 movieDB.delete_task(row[0])
 
             elif '480' in info.headers['Content-Disposition'] and '{0}-480'.format(row[2]) not in dp:
                 dp['{0}-480'.format(row[2])] = True
-                download_upload(link)
+                download_upload(link, row[2])
                 movieDB.delete_task(row[0])
 
                 
