af7129e67a2839eb1d9f6c8dfb202c0d0bf0b173
Sobhan Shafiei <soshika1010@gmail.com>
2022-09-03 21:14:17 +0430

    convert test again

--- test.py
+++ test.py
@@ -4,17 +4,53 @@
 from moviepy.editor import VideoFileClip
 from time import sleep
 
+import os, ffmpeg
+
+def compress_video(video_full_path, output_file_name, target_size):
+    # Reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+
+    probe = ffmpeg.probe(video_full_path)
+    # Video duration, in s.
+    duration = float(probe['format']['duration'])
+    # Audio bitrate, in bps.
+    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+    # Target total bitrate, in bps.
+    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
+
+    # Target audio bitrate, in bps
+    if 10 * audio_bitrate > target_total_bitrate:
+        audio_bitrate = target_total_bitrate / 10
+        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+            audio_bitrate = min_audio_bitrate
+        elif audio_bitrate > max_audio_bitrate:
+            audio_bitrate = max_audio_bitrate
+    # Target video bitrate, in bps.
+    video_bitrate = target_total_bitrate - audio_bitrate
+
+    i = ffmpeg.input(video_full_path)
+    ffmpeg.output(i, os.devnull,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                  ).overwrite_output().run()
+    ffmpeg.output(i, output_file_name,
+                  **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                  ).overwrite_output().run()
+
 if __name__ == "__main__":
-    full_video = "./itest.mp4"
-    current_duration = VideoFileClip(full_video).duration
-    divide_into_count = 5
-    single_duration = current_duration/divide_into_count
-    current_video = f"{current_duration}.mp4"
 
-    while current_duration > single_duration:
-        clip = VideoFileClip(full_video).subclip(current_duration-single_duration, current_duration)
-        current_duration -= single_duration
-        current_video = f"{current_duration}.mp4"
-        clip.to_videofile(current_video, codec="libx264", temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac')
+    compress_video('4176.312.mp4', 'output.mp4', 50 * 1000)
 
-        print("-----------------###-----------------")
+    # full_video = "./itest.mp4"
+    # current_duration = VideoFileClip(full_video).duration
+    # divide_into_count = 5
+    # single_duration = current_duration/divide_into_count
+    # current_video = f"{current_duration}.mp4"
+
+    # while current_duration > single_duration:
+    #     clip = VideoFileClip(full_video).subclip(current_duration-single_duration, current_duration)
+    #     current_duration -= single_duration
+    #     current_video = f"{current_duration}.mp4"
+    #     clip.to_videofile(current_video, codec="libx264", temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac')
+
+    #     print("-----------------###-----------------")
